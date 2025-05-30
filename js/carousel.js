7a4131e94a5355585ca092589dcbf920b561a45d
@LeonamAS <leonam.sales@ba.estudante.senai.br>
2025-05-30 12:00:29 -0300

    Aula 30/05

--- js/carousel.js
+++ js/carousel.js
@@ -31,34 +31,27 @@ class Carousel {
     }
 
     static Next() {
+        let sequencia = Carousel._sequence;
+        let carrossel = document.getElementById("carousel");
+        let imagem = document.getElementById("imagem");
+        let titulo = document.getElementById("carousel-title");
+        // let link = document.getElementById("link");
+
         // Div Carrosel
-        document.getElementById("carousel").style.height = "300px";
-        document.getElementById("carousel").style.display = "flex";
-        document.getElementById("carousel").style.justifyContent = "center";
+        carrossel.style.height = "70%";
+        carrossel.style.display = "flex";
+        carrossel.style.justifyContent = "center";
 
         // Imagem do carrossel
-        document.getElementById("imagem").style.width = "600px";
-        document.getElementById("imagem").style.padding = "20px";
-        document.getElementById("imagem").style.alignItems = "center";
+        imagem.style.width = "70%";
 
-        // Troca das imagens
-        if (Carousel._sequence == 0) {
-            this._sequence++
-            document.getElementById("imagem").src = carouselArr[0].image;
-            document.getElementById("carousel-title").innerHTML = carouselArr[0].title;
-            document.getElementById("link").href = carouselArr[0].url;
-        }
-        else if (Carousel._sequence == 1) {
-            this._sequence++
-            document.getElementById("imagem").src = carouselArr[1].image;
-            document.getElementById("carousel-title").innerHTML = carouselArr[1].title;
-            document.getElementById("link").href = carouselArr[1].url;
-        }
-        else {
+        imagem.src = carouselArr[sequencia].image;
+        titulo.innerHTML = carouselArr[sequencia].title;
+        // link.href = carouselArr[sequencia].url;
+        this._sequence++
+
+        if (this._sequence == this._size) {
             Carousel._sequence = 0;
-            document.getElementById("imagem").src = carouselArr[2].image;
-            document.getElementById("carousel-title").innerHTML = carouselArr[2].title;
-            document.getElementById("link").href = carouselArr[2].url;
         }
     }
 }
--- js/compare.js
+++ js/compare.js
@@ -1,40 +1,52 @@
 
 //car
 let carArr = [];
+let item = 0;
 
 class Car {
-   
-
-    constructor(nome, preco, alturaCacamba, alturaVeiculo, alturaSolo, capacidadeCarga, motor, potencia, volumeCacamba, roda, image){
-       
+    constructor(nome, preco, alturaCacamba, alturaVeiculo, alturaSolo, capacidadeCarga, motor, potencia, volumeCacamba, roda, image) {
+        this.nome = nome;
+        this.preco = preco;
+        this.alturaCacamba = alturaCacamba;
+        this.alturaVeiculo = alturaVeiculo;
+        this.alturaSolo = alturaSolo;
+        this.capacidadeCarga = capacidadeCarga;
+        this.motor = motor;
+        this.potencia = potencia;
+        this.volumeCacamba = volumeCacamba;
+        this.roda = roda;
+        this.image = image;
     }
-} 
+
+}
 
 // search on array if exist carClass returning 1 if not return -1
 function GetCarArrPosition(arr, carClass) {
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i].nome  === carClass.nome)
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].nome === carClass.nome)
             return i;
     }
     return -1;
 }
 
+
 function SetCarToCompare(el, carClass) {
-   
-    if(carClass instanceof Car){       
-        if(el.checked){
-                
-            
+    if (carClass instanceof Car) {
+        if (el.checked == true) {
+            carArr[item] = carClass;
+            item++;
+            console.log(carArr);
         } else {
-          
-        } 
+            carArr.shift();
+            console.log(carArr);
+        }
     } else {
         throw "You need set a Car Class";
     }
 }
 
 function ShowCompare() {
-    if(carArr.length < 2) {
+    if (carArr.length < 2) {
         alert("Precisa marcar 2 carros para apresentar a comparação");
         return;
     }
@@ -43,10 +55,11 @@ function ShowCompare() {
     document.getElementById("compare").style.display = "block";
 }
 
-function HideCompare(){
-    document.getElementById("compare").style.display = "none"; 
+function HideCompare() {
+    document.getElementById("compare").style.display = "none";
 }
 
 function UpdateCompareTable() {
-    
+
 }
+
